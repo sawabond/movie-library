c8af7efd6c648daa04ca205ed6c2d79eac39ca43
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2023-05-06 07:19:32 +0100

    Fixing folder errors

--- server/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('movies-db', 'user', 'pass', {
-  dialect: 'sqlite',
-  host: 'movies.sqlite',
-});
-
-module.exports = sequelize;
